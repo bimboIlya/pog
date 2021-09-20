d61718e81df5a84d18d9cf2266b27228eafa76e6
bimboIlya <moonl1ght24rus@gmail.com>
2021-09-21 00:25:09 +0700

    used common theme

--- yacr/app/src/main/java/com/bimboilya/yacr/MainActivity.kt
+++ yacr/app/src/main/java/com/bimboilya/yacr/MainActivity.kt
@@ -8,10 +8,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.bimboilya.common.ui.theme.PogTheme
 import com.bimboilya.yacr.navigation.NavCommandDispatcher
 import com.bimboilya.yacr.navigation.extenstions.composable
 import com.bimboilya.yacr.navigation.screens.authorization.AuthDirection
-import com.bimboilya.yacr.theme.YacrTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
         }
 
         setContent {
-            YacrTheme {
+            PogTheme {
                 Navigation(navCommandDispatcher)
             }
         }
