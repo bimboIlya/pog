94d12f3bbcf860f4674c5ef31794b33e2dad3943
bimboIlya <moonl1ght24rus@gmail.com>
2021-09-24 23:14:32 +0700

    added navigation in firebase app

--- firebase/app/src/main/java/com/bimboilya/firebase/MainActivity.kt
+++ firebase/app/src/main/java/com/bimboilya/firebase/MainActivity.kt
@@ -3,26 +3,57 @@ package com.bimboilya.firebase
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.bimboilya.common.navigation.compose.NavCommandDispatcher
+import com.bimboilya.common.navigation.compose.extenstions.composable
 import com.bimboilya.common.ui.theme.PogTheme
+import com.bimboilya.firebase.navigation.chooser.ChooserDirection
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var navCommandDispatcher: NavCommandDispatcher
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenResumed {
+            navCommandDispatcher.navCommandFlow.collect { command ->
+                command(this@MainActivity) }
+        }
+
         setContent {
             PogTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                Navigation(navCommandDispatcher)
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun Navigation(navCommandDispatcher: NavCommandDispatcher) {
+    val navController = rememberNavController()
+
+    LaunchedEffect(navCommandDispatcher) {
+        navCommandDispatcher.composableNavCommandFlow.collect { command ->
+            command(navController)
+        }
+    }
+
+    NavHost(
+        navController,
+        startDestination = ChooserDirection.route,
+    ) {
+        composable(ChooserDirection)
+    }
 }
+
