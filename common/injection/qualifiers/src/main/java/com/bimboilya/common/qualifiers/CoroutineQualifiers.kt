3471f50852d6bc88d21527578934a2bb1c8c8cb5
bimboIlya <moonl1ght24rus@gmail.com>
2021-09-26 23:04:54 +0700

    qualifiers module

---
+++ common/injection/qualifiers/src/main/java/com/bimboilya/common/qualifiers/CoroutineQualifiers.kt
@@ -0,0 +1,15 @@
+package com.bimboilya.common.qualifiers
+
+import javax.inject.Qualifier
+
+@Qualifier
+annotation class IoDispatcher
+
+@Qualifier
+annotation class DefaultDispatcher
+
+@Qualifier
+annotation class MainDispatcher
+
+@Qualifier
+annotation class AppScope
