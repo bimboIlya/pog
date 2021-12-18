f1a6bdc53c66d27d7b13ddc9a785fa516cef31ba
bimboIlya <moonl1ght24rus@gmail.com>
2021-12-18 18:50:33 +0700

    navsample app

---
+++ navsample/app/src/main/java/com/bimboilya/navsample/NavChooserActivity.kt
@@ -0,0 +1,10 @@
+package com.bimboilya.navsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class NavChooserActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
