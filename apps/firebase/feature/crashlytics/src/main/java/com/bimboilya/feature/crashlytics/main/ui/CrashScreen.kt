17f5eb6ea14f1c76cd28bb539ba4df3a35a2386e
bimboIlya <moonl1ght24rus@gmail.com>
2022-01-01 22:49:44 +0700

    moved apps to separate folder

---
+++ apps/firebase/feature/crashlytics/src/main/java/com/bimboilya/feature/crashlytics/main/ui/CrashScreen.kt
@@ -0,0 +1,52 @@
+package com.bimboilya.feature.crashlytics.main.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.bimboilya.feature.crashlytics.main.presentation.CrashViewModel
+
+@Composable
+fun CrashScreen(viewModel: CrashViewModel) {
+    CrashScreen(
+        onCrashClick = viewModel::crash,
+        onLogErrorClick = viewModel::logError,
+        onSettingsClick = viewModel::openSettings
+    )
+}
+
+@Composable
+private fun CrashScreen(
+    onCrashClick: () -> Unit,
+    onLogErrorClick: () -> Unit,
+    onSettingsClick: () -> Unit,
+) {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+        IconButton(onClick = onSettingsClick) {
+            Image(Icons.Default.Settings, contentDescription = null)
+        }
+    }
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Button(onClick = onCrashClick) {
+            Text(text = "Crash")
+        }
+
+        Button(onClick = onLogErrorClick) {
+            Text(text = "Log error")
+        }
+    }
+}
---
+++ apps/yacr/app/src/main/java/com/bimboilya/yacr/YacrActivity.kt
@@ -0,0 +1,56 @@
+package com.bimboilya.yacr
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.bimboilya.common.navigation.compose.NavCommandDispatcher
+import com.bimboilya.common.navigation.compose.extenstions.composable
+import com.bimboilya.common.ui.theme.PogTheme
+import com.bimboilya.yacr.navigation.screens.authorization.AuthDestination
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class YacrActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var navCommandDispatcher: NavCommandDispatcher
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenResumed {
+            navCommandDispatcher.navCommandFlow
+                .collect { it(this@YacrActivity) }
+        }
+
+        setContent {
+            PogTheme {
+                Navigation(navCommandDispatcher)
+            }
+        }
+    }
+}
+
+@Composable
+fun Navigation(navCommandDispatcher: NavCommandDispatcher) {
+    val navController = rememberNavController()
+
+    LaunchedEffect(navCommandDispatcher) {
+        navCommandDispatcher.composableNavCommandFlow.collect { command ->
+            command(navController)
+        }
+    }
+
+    NavHost(
+        navController,
+        startDestination = AuthDestination.getRoute(),
+    ) {
+        composable(AuthDestination)
+    }
+}
